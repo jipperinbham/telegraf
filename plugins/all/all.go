ffb7e7bba8ac2425f9d83a83a4564ca0d7a78ad9
JP <jp@mongohq.com>
2015-06-23 10:35:30 -0500

    reference my fork

--- plugins/all/all.go
+++ plugins/all/all.go
@@ -5,4 +5,5 @@ import (
 	_ "github.com/influxdb/telegraf/plugins/postgresql"
 	_ "github.com/influxdb/telegraf/plugins/redis"
 	_ "github.com/influxdb/telegraf/plugins/system"
+	_ "github.com/jipperinbham/telegraf/plugings/rethinkdb"
 )
